d758b24fdbbf75c6f5121f0fb4aadb1fc6ffe6db
Fedor Shapkin <fedorshapkin20090414@gmail.com>
2025-02-02 23:24:37 +0300

    some trash

--- src/index.js
+++ src/index.js
@@ -8,11 +8,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <App/>
+        <div>Приложение работает!</div>
+        {
+            React.createElement('button' {
+
+    })
+        }
     </React.StrictMode>
 );
 
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
